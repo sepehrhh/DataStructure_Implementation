9cb0136338f2264609eb6b107a64cec3fb80e5f0
Sepehrhh <se.hashemih@gmail.com>
2018-12-13 22:08:20 +0330

    A10

---
+++ A10/A10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var problem = new RabinKarp("");
+            problem.Solve("aaaaa", "baaaaaaa");
+        }
+    }
+}
